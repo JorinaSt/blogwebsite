98a498d4e659b022af2fb7d9523936bafde2ad36
Jorina <jorinasteenput@hotmail.com>
2024-05-13 16:22:13 +0200

    ready with first draft front-end, next step adding backend connection

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,11 +7,13 @@ export const Navbar = () => {
     <div className='navbar'>
         <div className="container">
             <div className="logo">
+              <Link to ="/">
               <img src={Logo} alt=""/>
+              </Link>
             </div>
             <div className="links"> 
             <Link className="link" to="/?cat=about">
-            <h6>ABOUT</h6>
+            <h6>HOME</h6>
           </Link>
           <Link className="link" to="/?cat=sport">
             <h6>SPORT</h6>
@@ -28,6 +30,9 @@ export const Navbar = () => {
 
           <span>Jorina</span>
           <span>Logout</span>
+          <Link className="link" to="/login">
+              Login
+            </Link>
       
           <span className="write">
             <Link className="link" to="/WritePost">
--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -1,36 +1,44 @@
 import React from 'react'
+import Jorina from '../img/Jorina.png'
+import Delete from '../img/delete.png'
+import Edit from '../img/edit.png'
+import { Link } from 'react-router-dom'
+import Menu from '../components/Menu'
 
-export const 
+ const 
 SinglePost = () => {
   return (
     <div className="single">
     <div className="content">
-      <img src={post?.img} alt="" />
+      <img src="https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
       <div className="user">
-        {post.userImg && <img
-          src={post.userImg}
+         <img
+          src= {Jorina} 
           alt=""
-        />}
+        />
         <div className="info">
-          <span>{post.username}</span>
-          <p>Posted {moment(post.date).fromNow()}</p>
-        </div>
-        {currentUser.username === post.username && (
-          <div className="edit">
-            <Link to={`/write?edit=2`} state={post}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img onClick={handleDelete} src={Delete} alt="" />
+        <span>Jorina</span>
+          <p>Posted 2 days ago</p>
           </div>
-        )}
-      </div>
-      <h1>{post.title}</h1>
-      <p
-        dangerouslySetInnerHTML={{
-          __html: DOMPurify.sanitize(post.desc),
-        }}
-      ></p>      </div>
-    <Menu cat={post.cat}/>
+          <div className="edit">
+            <Link to={'/WritePost?edit=2'}>
+            <img src={Edit} alt="" />
+            </Link> 
+            <img src={Delete} alt="" />
+            </div>
+        </div>
+        <h1> Lorem ipsum dolor sit amet consectetur.</h1>
+        <p> 
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias consequatur veritatis error sed numquam reprehenderit assumenda, saepe deleniti sequi aliquid vel? Ex omnis amet ipsam iste deleniti? Eaque, tempore iure?
+          <br /> 
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi et fuga placeat esse harum assumenda molestias tempore, ratione officia mollitia, impedit illum quae laboriosam debitis! Praesentium, consequatur? Ipsam, saepe atque.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+    </div>
+   <Menu />
   </div>
   )
 }
