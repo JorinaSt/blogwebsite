91d7ad66259006efded3f283ed9e26a51597aead
Jorina <jorinasteenput@hotmail.com>
2024-05-13 12:29:36 +0200

    finished the homepage, footer and started at singlepost

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import Logo from '../img/logo.png'
 
 export const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="container">
-            <div className="logo">Lama Social</div>
+            <div className="logo">
+              <img src={Logo} alt=""/>
+            </div>
             <div className="links"> 
-            <Link className="link">
+            <Link className="link" to="/?cat=about">
             <h6>ABOUT</h6>
           </Link>
-          <Link className="link">
+          <Link className="link" to="/?cat=sport">
             <h6>SPORT</h6>
           </Link>
-          <Link className="link">
+          <Link className="link" to="/?cat=code">
             <h6>CODE</h6>
           </Link>
-          <Link className="link">
+          <Link className="link" to="/?cat=food">
             <h6>FOOD</h6>
           </Link>
-          <Link className="link">
+          <Link className="link" to="/?cat=design">
             <h6>DESIGN</h6>
           </Link>
+
+          <span>Jorina</span>
+          <span>Logout</span>
       
           <span className="write">
             <Link className="link" to="/WritePost">
